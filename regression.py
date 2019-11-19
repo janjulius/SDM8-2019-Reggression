39f0856ed16a66ed0f8ff5a3949776cb0268623a
dwarfexop <lars-schipper@hotmail.com>
2019-11-19 11:01:46 +0100

    Add colors, better logging, payload check

--- regression.py
+++ regression.py
@@ -1,4 +1,8 @@
 import paho.mqtt.client as mqtt
+import sys
+
+try: color = sys.stdout.shell
+except AttributeError: raise RuntimeError("Use IDLE")
 
 group_no = 8#input("Group no: ")
 
@@ -17,6 +21,10 @@ class Sub_Part:
     def __init__(self, max, next):
         self.max = max
         self.next = next
+
+class Payload_Part:
+    def __init__(self, max):
+        self.max = max
 
 class Sub:
     def __init__(self, parent, max):
@@ -38,6 +46,9 @@ def is_part(val):
 def is_multi_part(val):
     return type(val) == list and len(list(val)) >= 0 and type(val[0]) == Part
 
+def is_payload_part(val):
+    return type(val) == Payload_Part
+
 def is_valid_part(split_topic, part):
     return part.value == split_topic
 
@@ -47,58 +58,83 @@ def intTryParse(value):
     except ValueError:
         return value, False
 
-def check_valid_part(split_topic, current_part):    
+def check_valid_part(split_topic, current_part):
+    #  STRING (component_type, lane_type) #
     if is_multi_part(current_part):
         for current_multi_part in current_part:
             if is_valid_part(split_topic, current_multi_part):
+                # Valid string part
                 return current_multi_part.next, True
-        print("ERROR: invalid part, " + current_part)
+        color.write(f"ERROR: invalid string, {split_topic}\n", "COMMENT")
         return None, False
-                    
+
+    # INTEGER (group_id, component_id) # 
     elif is_number_part(current_part):
         split_topic, success = intTryParse(split_topic)
         if success:
             if int(split_topic) <= current_part.max:
                 if current_part.subs == None:
+                    # No subs for group
                     return current_part.next, True
                 else:
                     for sub in current_part.subs:
                         if sub.parent == int(split_topic):
+                            # Return valid subs for group (or NULL)
                             return Sub_Part(sub.max, current_part.next), True
-                    print("ERROR: invalid number, " + current_part)
-                    return None, False
+
+                    # No subs for group so it should be NULL
+                    return Sub_Part(None, current_part.next), True
             else:
-                print("ERROR: invalid number, " + current_part)
+                color.write(f"ERROR: invalid group, {split_topic}\n", "COMMENT")
                 return None, False
         else:
-           print("ERROR: invalid int in number, " + current_part)
+           color.write(f"ERROR: not a number, {split_topic}\n", "COMMENT")
            return None, False
 
-    elif is_sub_part(current_part):
+    # -- PAYLOAD --
+    elif is_payload_part(current_part):
         split_topic, success = intTryParse(split_topic)
         if success:
             if int(split_topic) <= current_part.max:
+                # Done, valid topic
+                return None, True
+            else:
+                color.write(f"ERROR: invalid payload, {split_topic}\n", "COMMENT")
+                return None, False
+        else:
+           color.write(f"ERROR: payload not a number, {split_topic}\n", "COMMENT")
+           return None, False
+        
+    # -- SUB GROUP --
+    elif is_sub_part(current_part):
+        split_topic, success = intTryParse(split_topic)
+        if success:
+            if current_part.max is not None and int(split_topic) <= current_part.max:
+                # Valid sup group, check next part
                 return current_part.next, True
             else:
-                print("ERROR: invalid number, " + current_part)
-                return None, False
+                color.write(f"ERROR: group does not have {split_topic} as subgroup\n", "COMMENT")
+                return None, False  
         elif split_topic == 'NULL':
+            # Subgroup can be NULL
             return current_part.next, True
         else:
-           print("ERROR: invalid int in number, " + current_part)
-           return None, False
-                
+            color.write(f"ERROR: invalid value in sub group, {split_topic}\n", "COMMENT")
+            return None, False
+
+    # -- STRING (component_type, lane_type) --
     elif is_part(current_part):
         if is_valid_part(split_topic, current_part):
+            # Valid string part
             return current_part.next, True
         else:
-            print("ERROR: invalid part, " + current_part)
+            color.write(f"ERROR: invalid string, {split_topic}\n", "COMMENT")
             return None, False
 
 
 
 def get_component(name, max_id, max_payload):
-    return Part(name, Number_Part(max_id, Number_Part(max_payload, None, None), None))
+    return Part(name, Number_Part(max_id, Payload_Part(max_payload), None))
             
 sensor_motorised = get_component("sensor", 1, 1)
 sensor = get_component("sensor", 0, 1)
@@ -115,13 +151,13 @@ motorised_part = Part("motorised", motorised_number_part)
 foot_number_part = Number_Part(7, [traffic_light, sensor], [Sub(0, 1), Sub(1, 1), Sub(4, 2), Sub(5, 2)])
 foot_part = Part("foot", foot_number_part)
 
-cycle_number_part = Number_Part(5, [traffic_light, sensor_motorised], [Sub(3, 1)])
+cycle_number_part = Number_Part(5, [traffic_light, sensor], [Sub(3, 1)])
 cycle_part = Part("cycle", cycle_number_part)
 
-vessel_number_part = Number_Part(1, [traffic_light, sensor_motorised, warning_light, barrier_vessel], None)
+vessel_number_part = Number_Part(1, [traffic_light, sensor, warning_light, barrier_vessel], [Sub(0, None)])
 vessel_part = Part("vessel", vessel_number_part)
 
-track_number_part = Number_Part(1, [traffic_light, sensor_motorised, warning_light, barrier_track], None)
+track_number_part = Number_Part(1, [traffic_light, sensor, warning_light, barrier_track], [Sub(0, None)])
 track_part = Part("track", track_number_part)
 
 valid_parts = [motorised_part, foot_part, cycle_part, vessel_part, track_part]
@@ -139,7 +175,9 @@ def on_connect(client, userdata, flags, rc):
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
+    payload = msg.payload.decode('utf-8')
+    color.write("\n#############################################\n\n", "KEYWORD")
+    print(f"TOPIC: {msg.topic} - PAYLOAD: {payload}")
 
     split_topics = msg.topic.split('/', -1)
     split_topics = list(filter(None, split_topics))
@@ -148,7 +186,7 @@ def on_message(client, userdata, msg):
     split_topics_length = len(split_topics)
         
     if split_topics_length == 5:
-        split_topics.append(msg.payload.decode('utf-8'))
+        split_topics.append(payload)
         current_part = valid_parts
         success = False
         
@@ -159,7 +197,7 @@ def on_message(client, userdata, msg):
                 break
 
         if success:
-            print("OK: valid topic")
+            color.write("OK: valid topic\n", "STRING")
     else:
         print('ERROR: invalid topic length')
     
--- send_test.py
+++ send_test.py
@@ -1,13 +1,27 @@
 import paho.mqtt.client as mqtt #import the client1
+from random import randrange
 
 group_no = 8#input("Group no: ")
 
-topic=f"{group_no}/foot/0/NULL/sensor/0/"
+topics=[f"{group_no}/foot/0/NULL/sensor/0/",
+f"{group_no}/foot/0/1/sensor/0/",
+f"{group_no}/foot/0/1/sensor/0/",
+f"{group_no}/vessel/0/NULL/sensor/0/",
+f"{group_no}/vessel/0/1/sensor/0/",
+f"{group_no}/motorised/0/1/sensor/0/",
+f"{group_no}/motorised/0/NULL/traffiac_light/0/",
+f"{group_no}/vessel/0/1/sensor/0/",
+f"{group_no}/vessel/0/1/sensor/0/",
+f"{group_no}/foot/3/NULL/sensor/0/",
+f"{group_no}/foot/2/1/traffic_light/0/",
+f"{group_no}/fogot/4/NULL/traffic_light/0/"]
+
 #broker = "test.mosquitto.org"
 broker = "62.210.180.72"
 print("creating new instance")
 client = mqtt.Client("P1") #create new instance
 print("connecting to broker")
 client.connect(broker) #connect to broker
-print("Publishing message to topic",topic)
-client.publish(topic,'1')
+for topic in topics:
+	print("Publishing message to topic",topic)
+	client.publish(topic,randrange(3))
