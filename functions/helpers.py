3c7e3857654c7a15e89da41124258f2c01c8becc
dwarfexop <lars-schipper@hotmail.com>
2019-12-12 23:32:26 +0100

    Fix indents

--- functions/helpers.py
+++ functions/helpers.py
@@ -2,6 +2,6 @@ from cust_logging import print_seperator, log_warning
 
 # Check if statement (lambda) is met, if so prints warning
 def check_statement(statement, message, topic):
-    if statement():
-        print_seperator()
-        log_warning(f'{message}\n(CALLBACK FROM: {topic}) ')
+	if statement():
+		print_seperator()
+		log_warning(f'{message}\n(CALLBACK FROM: {topic}) ')
